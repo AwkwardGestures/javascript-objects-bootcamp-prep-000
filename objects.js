9e9c108556b26b47d228a443644aff3f277d07e4
Bill Monney <billyemmel@gmail.com>
2018-02-02 21:21:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -87,8 +87,9 @@ delete meals.dinner; //will return as true if the value pair associated with 'di
 
 Object.assign({}, meals, { breakfast: ['oatmeal', 'banana'] });
     //returns a new copy of the meals object with the amended breakfast values; meals will still return the original values
-    
-var playlist = {
+
+var artistName =     
+var playlist = { 
   
 };
 
